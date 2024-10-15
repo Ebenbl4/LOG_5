c3e6c4a99c3318855310bb7162604d0e5077eeab
Alexey Rodyonov <alesha20045@mail.ru>
2024-10-15 09:57:09 +0300

    Задание 1

---
+++ LOG_L_5_1/LOG_L_5_1.cpp
@@ -0,0 +1,158 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <malloc.h>
+#include <random>
+#include <locale>
+
+void initialize_graph(int size, int_fast8_t** graph_pointer, float edge_probability);
+int_fast8_t** create_graph(int size);
+void print_graph(int size, int_fast8_t** graph_pointer);
+void graph_info(int size, int_fast8_t** graph_pointer);
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int_fast8_t** graph;
+	int size;
+	float edge_probability;
+
+	fprintf(stdout, "Введите количество вершин графа: ");
+	if (!scanf("%d", &size) || size <= 1) {
+		fprintf(stderr, "Ошибка ввода размера массива");
+		exit(1);
+	}
+
+	fprintf(stdout, "Введите вероятность создания ребра (от 0 до 100): ");
+	if (!scanf("%f", &edge_probability) || edge_probability < 0 || edge_probability > 100) {
+		fprintf(stderr, "Ошибка ввода вероятности");
+		exit(1);
+	}
+
+	graph = create_graph(size);
+	initialize_graph(size, graph, edge_probability);
+	printf("Матрица смежности: \n\n");
+	print_graph(size, graph);
+	graph_info(size, graph);
+
+	for (int i = 0; i < size; i++) {
+		free(graph[i]);
+	}
+	free(graph);
+
+	return 0;
+}
+
+int_fast8_t** create_graph(int size) {
+	int_fast8_t** array = (int_fast8_t**)malloc(sizeof(int_fast8_t*) * size);
+	for (int i = 0; i < size; i++) {
+		array[i] = (int_fast8_t*)malloc(sizeof(int_fast8_t) * size);
+	}
+	if (array == NULL) {
+		fprintf(stderr, "Ошибка создания массива");
+		exit(1);
+	}
+	return array;
+}
+
+void initialize_graph(int size, int_fast8_t** graph_pointer, float edge_probability) {
+	srand((unsigned int)time(NULL));
+	for (int i = 0; i < size; i++) {
+		for (int j = i + 1; j < size; j++) {
+			if ((float)rand() / (RAND_MAX / 100) < edge_probability) {
+				graph_pointer[i][j] = 1;
+				graph_pointer[j][i] = 1;
+			}
+			else {
+				graph_pointer[i][j] = 0;
+				graph_pointer[j][i] = 0;
+			}
+		}
+		graph_pointer[i][i] = 0;
+	}
+}
+
+void print_graph(int size, int_fast8_t** graph_pointer) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			fprintf(stdout, "%d ", graph_pointer[j][i]);
+		}
+		fprintf(stdout, "\n");
+	}
+}
+
+void graph_info(int size, int_fast8_t** graph_pointer) {
+    int edge_count = 0;
+    int* isolated_vertices = (int*)calloc(size, sizeof(int));
+    int* leaf_vertices = (int*)calloc(size, sizeof(int));
+    int* dominating_vertices = (int*)calloc(size, sizeof(int));
+    int isolated_count = 0, leaf_count = 0, dominating_count = 0;
+
+    for (int i = 0; i < size; i++) {
+        int degree = 0;
+        int is_isolated = 1;
+        int is_dominating = 1;
+        for (int j = 0; j < size; j++) {
+            if (graph_pointer[i][j] == 1) {
+                is_isolated = 0;
+                degree++;
+                if (i < j) {
+                    edge_count++;
+                }
+            }
+            else if (i != j) {
+                is_dominating = 0;
+            }
+        }
+        if (is_isolated) {
+            isolated_vertices[isolated_count++] = i + 1;
+        }
+        else if (degree == 1) {
+            leaf_vertices[leaf_count++] = i + 1;
+        }
+        if (is_dominating) {
+            dominating_vertices[dominating_count++] = i + 1;
+        }
+    }
+
+    printf("\nРазмер графа: %d\n", edge_count);
+
+    printf("\nИзолированные вершины:\n");
+    if (isolated_count == 0) {
+        printf("Изолированных вершин нет\n");
+    }
+    else {
+        for (int i = 0; i < isolated_count; i++) {
+            printf("%d ", isolated_vertices[i]);
+        }
+        printf("\n");
+    }
+
+    printf("\nКонцевые вершины:\n");
+    if (leaf_count == 0) {
+        printf("Концевых вершин нет\n");
+    }
+    else {
+        for (int i = 0; i < leaf_count; i++) {
+            printf("%d ", leaf_vertices[i]);
+        }
+        printf("\n");
+    }
+
+    printf("\nДоминирующие вершины:\n");
+    if (dominating_count == 0) {
+        printf("Доминирующих вершин нет\n");
+    }
+    else {
+        for (int i = 0; i < dominating_count; i++) {
+            printf("%d ", dominating_vertices[i]);
+        }
+        printf("\n");
+    }
+
+    free(isolated_vertices);
+    free(leaf_vertices);
+    free(dominating_vertices);
+}
